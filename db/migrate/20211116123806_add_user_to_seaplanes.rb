a3b045a8db8d9087a39d223958eda7e73b3629a7
Giuseppe <90353257+Mithrandir90@users.noreply.github.com>
2021-11-16 14:23:08 +0000

    null:false added to add user migration

--- db/migrate/20211116123806_add_user_to_seaplanes.rb
+++ db/migrate/20211116123806_add_user_to_seaplanes.rb
@@ -1,5 +1,5 @@
 class AddUserToSeaplanes < ActiveRecord::Migration[6.0]
   def change
-    add_reference :seaplanes, :user, foreign_key: true
+    add_reference :seaplanes, :user, null: false, foreign_key: true
   end
 end
