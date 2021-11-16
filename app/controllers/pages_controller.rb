3c6c89e3a0b7801cc113874b604d029422a0ce59
arnold ho <tchoarnold@gmail.com>
2021-11-16 16:06:29 +0000

    finished search functionality in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @seaplane_category = Seaplane.distinct.pluck(:category)
   end
 
   def search
-    price_query = params[:price]
-    category_query = params[:category]
+    @seaplane_category = Seaplane.distinct.pluck(:category)
+    price_query = params[:search][:price]
+    category_query = params[:search][:category]
+    capacity_query = params[:search][:capacity]
     @seaplanes = Seaplane.where(category: category_query)
     @seaplanes = @seaplanes.where("price <= ?", price_query)
+    @seaplane = @seaplanes.where("capacity >= ?", capacity_query)
     render :home
   end
 end
