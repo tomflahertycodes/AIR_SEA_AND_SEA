5d55bdfb830409827531d59ba5936bf2299c94fa
thomas flaherty <thomasflaherty990@gmail.com>
2021-11-19 14:23:59 +0000

    map icons added

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,2 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .scss
+//= link_directory ../stylesheets .css
--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 export default class extends Controller {
   static values = {
@@ -15,12 +16,25 @@ export default class extends Controller {
     });
     this._addMarkersToMap();
     this._fitMapToMarkers();
+    this.map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+    }));
   }
 
   _addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      new mapboxgl.Marker()
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
+      const customMarker = document.createElement('div');
+      customMarker.className = 'marker';
+      customMarker.style.backgroundImage = `url('${marker.image_url}')`;
+      customMarker.style.backgroundSize = 'contain';
+      customMarker.style.width = '25px';
+      customMarker.style.height = '25px';
+      new mapboxgl.Marker(customMarker)
         .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
         .addTo(this.map);
     });
   }
